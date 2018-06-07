3f1084790cc80b9a989cf790ee2e43efee971124
Nathan Smith <nathan@praisetopia.org>
2018-06-06 22:54:54 -0500

    Zones!

--- py5Q.py
+++ py5Q.py
@@ -17,14 +17,45 @@ EFFECTS = "{0}/api/1.0/DK5QPID/effects"
 CLIENTID = "YOUR_ID"
 CLIENTSECRET = "YOUR_SECRET"
 
+BADZONES = [0, 18, 19, 20, 21]
+
+
+class Zone:
+    """Represents a single zone/key."""
+    def __init__(self, zoneId, zoneCode, zoneName):
+        print(zoneId)
+        self.zoneId = zoneId
+        self.zoneCode = zoneCode
+        self.zoneName = zoneName
+        if zoneId not in BADZONES:
+            self.zoneCoordX = zoneId % 24
+            self.zoneCoordY = (zoneId - 24) // 24
+        elif zoneId != 0:
+            tempId = zoneId + 25
+            self.zoneCoordX = tempId % 24
+            self.zoneCoordY = (tempId - 24) // 24
+        else:
+            self.zoneCoordX = 0
+            self.zoneCoordY = 0
+        self.zoneCoords = "{0},{1}".format(self.zoneCoordX, self.zoneCoordY)
+    
+    def __eq__(self, other):
+        if isinstance(other, str) and str(other).startswith("0,"):
+            return self.zoneId == 0
+        return other == self.zoneId or other == self.zoneCode or other == self.zoneName or other == self.zoneCoords
+
+    def __str__(self):
+        return self.zoneId
+
 
 class EndpointList:
+    """Keeps all endpoints, sets them to be local or remote."""
     def __init__(self, mode, port):
         if mode != "remote":
             source = LOCAL.format(str(port))
         else:
             source = REMOTE
-        
+
         self.auth = AUTH.format(source)
         self.signals = SIGNALS.format(source)
         self.authorized_clients = AUTHORIZED_CLIENTS.format(source)
@@ -36,6 +67,7 @@ class EndpointList:
 
 
 class QSession:
+    """Gets OAuth token and automatically refreshes token when needed"""
     def __init__(self, clientId, secret, endpoint, mode="secret"):
         self._endpoint = endpoint
         self._clientId = clientId
@@ -44,17 +76,20 @@ class QSession:
 
     @property
     def token(self):
+        """Get the token, unless it has expired. Then, get a new token."""
         if(time.time() > self._expiresAt):
             self.refreshToken()
         return self._accessToken
 
     def updateTokens(self, tokens):
+        """Set properties from server response."""
         self._accessToken = tokens["access_token"]
         self._refreshToken = tokens["refresh_token"]
         self._userId = tokens["user_id"]
         self._expiresAt = time.time() + tokens["expires_in"] - 1000
 
     def authenticate(self, clientId, secret, mode):
+        """ClientId + Secret auth is preferred"""
         if mode != "secret":
             return self.authenticateEmailPassword(clientId, secret)
         else:
@@ -67,7 +102,7 @@ class QSession:
             "grant_type": "client_credentials"
         }
         return self.getTokens(data)
-    
+
     def authenticateEmailPassword(self, email, password):
         data = {
             "email": email,
@@ -83,7 +118,7 @@ class QSession:
             return json.loads(r.content)
         except TypeError:
             return json.loads(r.content.decode('utf-8'))
-    
+
     def refreshToken(self):
         data = {
             "client_id": self._clientId,
@@ -92,13 +127,27 @@ class QSession:
         }
         tokens = self.getTokens(data)
         self.updateTokens(tokens)
-        
+
 
 class pyQ:
+    """Main class. Handles creating the session and creating signals."""
     def __init__(self, mode="remote", port=27301, clientId=CLIENTID, secret=CLIENTSECRET):
         self.endpoints = EndpointList(mode, port)
         self.session = QSession(clientId, secret, self.endpoints.auth)
-    
+        self.zones = self._getZones()
+
+    def _getZones(self):
+        """Returns a list of Zone objects"""
+        zones = []
+        r = requests.get(self.endpoints.zones, headers=self.getHeaders())
+        try:
+            jsonZones = json.loads(r.content)
+        except TypeError:
+            jsonZones = json.loads(r.content.decode('utf-8'))
+        for jsonZone in jsonZones:
+            zones.append(Zone(jsonZone["id"], jsonZone["code"], jsonZone["name"]))
+        return zones
+
     def getHeaders(self):
         return {
             "Content-Type": "application/json",
@@ -120,30 +169,36 @@ class pyQ:
             "isArchived": isArchived,
             "isMuted": isMuted
         }
-        r = requests.post(self.endpoints.signals, headers=self.getHeaders(), json=data)
+        r = requests.post(self.endpoints.signals,
+                          headers=self.getHeaders(), json=data)
         try:
             return json.loads(r.content)["id"]
         except TypeError:
             return json.loads(r.content.decode('utf-8'))["id"]
-    
-    def batchSignal(self, zones, color, name="pyQ Signal", effect="SET_COLOR", message=None, action=None, shouldNotify=None, isRead=None, isArchived=None, isMuted=None):
+
+    def batchSignal(self, zones, color, name="pyQ Signal", effect="SET_COLOR", message=None, action=None,
+                    shouldNotify=None, isRead=None, isArchived=None, isMuted=None):
         signals = []
         for zone in zones:
-            signals.append(self.signal(zone, color, name, effect, message, action, shouldNotify, isRead, isArchived, isMuted))
+            signals.append(self.signal(zone, color, name, effect, message,
+                                       action, shouldNotify, isRead, isArchived, isMuted))
         return signals
-    
-    def batchSignalRange(self, x, y, color, name="pyQ Signal", effect="SET_COLOR", message=None, action=None, shouldNotify=None, isRead=None, isArchived=None, isMuted=None):
+
+    def batchSignalRange(self, x, y, color, name="pyQ Signal", effect="SET_COLOR", message=None, action=None,
+                         shouldNotify=None, isRead=None, isArchived=None, isMuted=None):
         signals = []
-        for i in range(x[0], x[1]+1):
-            for j in range(y[0], y[1]+1):
-                signals.append(self.signal("{0},{1}".format(i, j), color, name=name, effect=effect, message=message, action=action, shouldNotify=shouldNotify, isRead=isRead, isArchived=isArchived, isMuted=isMuted))
+        for i in range(x[0], x[1] + 1):
+            for j in range(y[0], y[1] + 1):
+                signals.append(self.signal("{0},{1}".format(i, j), color, name=name, effect=effect, message=message,
+                                           action=action, shouldNotify=shouldNotify, isRead=isRead,
+                                           isArchived=isArchived, isMuted=isMuted))
         return signals
-    
+
     def archive(self, signalId):
         data = {
             "isArchived": True
         }
         return requests.patch("{0}/{1}".format(self.endpoints.signals, signalId), headers=self.getHeaders(), json=data)
-    
+
     def delete(self, signalId):
         return requests.delete("{0}/{1}".format(self.endpoints.signals, signalId), headers=self.getHeaders())
